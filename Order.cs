0853807a6073fc441b97f527e7b7f46cbe718214
GeorgesSaad <30405374+GeorgesSaad@users.noreply.github.com>
2017-07-24 12:32:16 +0300

    Update Order.cs

--- Order.cs
+++ Order.cs
@@ -1,8 +1,14 @@
 public class Order
 {
+  public Order()
+  {
+    orderLines = new List<OrderLine>();
+  }
+  
   public Order(Customer c)
   {
     customer = c;
+    orderLines = new List<OrderLine>();
   }
 
   public string Number { get; set;}
@@ -20,6 +26,23 @@ public class Order
       customer = value;
     }
   }
+  private List<OrderLine> lines;
+  public List<OrderLine> Lines
+  {
+    get
+    {
+      return orderLines;
+    }
+  }
   
-
+  public Decimal Total
+  {
+    get
+    {
+      Decimal total = 0;
+      foreach(var line in Lines)
+        total += line.Quantity * line.UnitPrice;
+      return total;
+    }
+  }
 }
